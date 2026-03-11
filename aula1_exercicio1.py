a98e9d66370319ecf505d82d855afde9258c26bf
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-11 19:04:37 -0300

    First commit

---
+++ aula1_exercicio1.py
@@ -0,0 +1,4 @@
+nome = 'William César dos Santos'
+idade = 36
+
+print('Olá, meu nome é ', nome, ' e tenho a idade: ', idade)
---
+++ ola_mundo.py
@@ -0,0 +1,18 @@
+print('Olá, mundo!')
+
+# Variáveis
+# Tipo de dado: inteiro
+idade = 36
+# Tipo de dado: float
+salario = 1545.15
+# Tipo de dado: string
+nome = 'William César dos Santos'
+# Tipo de dado: booleano (1 ou 0, verdadeiro ou falso)
+estudante = False
+
+'''
+Este programa é um exemplo simples de como criar variáveis em Python e imprimir uma mensagem na tela.
+As variáveis são usadas para armazenar informações que podem ser usadas posteriormente no programa.
+'''
+
+print('Meu nome é:', nome, 'e tenho a idade:', idade, sep=' ', end='\n')
