7a65c044be736912d3da058632e1473a695678b2
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-16 19:58:04 -0300

    Exercises

---
+++ aula3_exercicio5.py
@@ -0,0 +1,12 @@
+'''
+Peça ao usuário para informar a nota do aluno e também a 
+  frequência nas aulas.
+A frequência será informada em quantidade de aulas assistidas e o 
+  total de aulas.
+Para que o aluno seja aprovado, ele precisa ter uma nota 
+  maior ou igual a 8 e uma frequência maior ou igual a 85%.
+Caso o aluno tenha nota inferior a 8. Porém acima de 6 e com 
+  frequência maior ou igual a 85%, o aluno estará de recuperação.
+Caso a nota do aluno seja inferior a 6, ele esta reprovado, 
+  independente da frequência.
+'''
