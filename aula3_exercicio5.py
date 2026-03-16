11674e0d85d3baec1445b788879e70b541cfe584
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-16 20:18:57 -0300

    Exercise

--- aula3_exercicio5.py
+++ aula3_exercicio5.py
@@ -7,6 +7,7 @@ Para que o aluno seja aprovado, ele precisa ter uma nota
   maior ou igual a 8 e uma frequência maior ou igual a 85%.
 Caso o aluno tenha nota inferior a 8. Porém acima de 6 e com 
   frequência maior ou igual a 85%, o aluno estará de recuperação.
+  Se a frequência for inferior a 85%, o aluno estará reprovado,
 Caso a nota do aluno seja inferior a 6, ele esta reprovado, 
   independente da frequência.
 '''
