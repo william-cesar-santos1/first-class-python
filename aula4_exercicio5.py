3d733326ee923f76e4da97e56db0701f14e9324f
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-18 21:34:22 -0300

    Exercise

---
+++ aula4_exercicio5.py
@@ -0,0 +1,24 @@
+'''
+Você deve criar um programa que analise dados de vendas de uma loja.
+
+Entrada
+
+O programa deve trabalhar com uma lista de vendas no seguinte formato:
+vendas = [
+    ["Notebook", 3500, 2],
+    ["Mouse", 80, 10],
+    ["Teclado", 150, 5],
+    ["Monitor", 900, 3],
+    ["Mouse", 80, 2],
+    ["Notebook", 3500, 1]
+]
+
+Cada item da lista representa:
+[nome_do_produto, preço_unitário, quantidade_vendida]
+
+
+Desafios
+ - Faturamento total. Calcule o faturamento total da loja.
+ - Ticket médio. Calcule o ticket médio total.
+ - Pedido mais caro.
+'''
