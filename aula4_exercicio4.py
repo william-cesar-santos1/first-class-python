e9598436b263bffe6834de05bef0acb7801a8ea7
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-18 21:20:08 -0300

    Exercise

---
+++ aula4_exercicio4.py
@@ -0,0 +1,9 @@
+'''
+Peça ao usuário que informe o próprio nome.
+Conte quantas vogais existem no nome informado.
+Exemplo: Se o usuário informar o nome "William", o resultado deve ser:
+O nome "William" possui 3 vogais.
+
+Precisa utilizar for para resolver.
+for letra in nome:
+'''
