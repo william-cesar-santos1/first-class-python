73c2c6748e85f1065c0ef5f69731f0139d155548
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-23 21:59:42 -0300

    update

--- aula5_exercicio6.py
+++ aula5_exercicio6.py
@@ -21,9 +21,26 @@ questoes = {
     }
 }
 
-for chave, questao in questoes.items():
+for chave, dicionario_questao in questoes.items():
     # questão também é um dicionário, então podemos acessar suas chaves e valores
-    print(questao['pergunta'])
+    print(dicionario_questao['pergunta'])
     # opcoes é uma lista, você pode iterar sobre ela para mostrar as opções
-    opcoes = questao['opcoes']
-    print(opcoes)
+    
+    opcoes = dicionario_questao['opcoes']
+    for index, opcao in enumerate(opcoes, start=1):
+        print(f'{index} - {opcao}')
+    
+    opcao_escolhida = int(input('Digite o número da opção correta: '))
+    
+    resposta_correta = dicionario_questao['resposta']
+    if opcoes[opcao_escolhida - 1] == resposta_correta:
+        print('Resposta correta!')
+    else:
+        print('Resposta incorreta. A resposta correta é:', resposta_correta)
+        
+'''
+Plus++++
+Adicione uma contagem de acertos e erros para o usuário.
+Aumente a quantidade de perguntas.
+Se o usuário errar mais que três vezes, o programa deve encerrar.
+'''
