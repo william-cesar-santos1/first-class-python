5bfccad1ee96b177ce8b7c0e6ede92df4bab051c
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-25 19:56:53 -0300

    Exercises

---
+++ aula6_exercicio2.py
@@ -0,0 +1,9 @@
+'''
+Dado a lista de produtos abaixo. Reajuste o preço de venda de todos em 10%.
+'''
+produtos = [
+    {'nome': 'Notebook', 'preco': 2500.00},
+    {'nome': 'Monitor', 'preco': 800.00},
+    {'nome': 'Teclado', 'preco': 150.00},
+    {'nome': 'Mouse', 'preco': 100.00}
+]
--- dicionario.py
+++ dicionario.py
@@ -25,9 +25,40 @@ dicionario = {
     'hobbies': ['programação', 'futebol', 'música']
 }
 
+# Acesso a valores do dicionário usando chaves
+print(dicionario['nome'])  # Acessa o valor associado à chave 'nome'
+print(dicionario['idade'])  # Acessa o valor associado à chave 'idade'
+# Acesso seguro usando o método get, que retorna None ou um valor padrão se a chave não existir
+print(dicionario.get('data_nascimento', '2020-01-01'))
+
+# Adicionando um novo par chave-valor ao dicionário
+dicionario['data_nascimento'] = '2000-01-01'
+print(dicionario['data_nascimento'])
+
+# Percorrer por chave e valor usando items()
 for chave, valor in dicionario.items():
     print(f'{chave=},{valor=}')
 
+# Percorrer por chave
 for chave in dicionario:
     print(f'{chave=}')
     print(f'{dicionario[chave]=}')
+
+# Removendo um par chave-valor do dicionário usando del
+print('Removendo a chave "estudante" usando del')
+del dicionario['estudante']
+print(dicionario)
+
+print('Removendo a chave "salario" usando pop')
+dicionario.pop('salario')  # Remove o par chave-valor associado à chave 'salario'
+print(dicionario)
+
+# dicionario.keys() retorna uma visão das chaves do dicionário
+print(dicionario.keys())
+
+# dicionario.values() retorna uma visão dos valores do dicionário
+print(dicionario.values())
+
+print('Criando um novo dicionário com chaves em maiúsculas usando compreensão de dicionários')
+novo_dicionario = { chave.upper() : valor for chave, valor in dicionario.items()}
+print(novo_dicionario)
