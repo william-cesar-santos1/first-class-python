52b69b2ee141a314e2b792e96587b0c18e92c59f
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-03-25 21:24:03 -0300

    Exercises

---
+++ aula6_exercicio7.py
@@ -0,0 +1,6 @@
+'''
+Dado uma quantidade indefinida de números, crie uma função capaz de 
+  identificar o maior número par e o menor número impar.
+A função deve retornar ambos os números encontrados.
+'''
+numeros = [3, 1, 4, 7, 5, 9, 2, 8, 6]
