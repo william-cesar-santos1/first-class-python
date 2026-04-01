03ba5945b9c670a09a839f8aa72a66aa46652420
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-04-01 20:10:54 -0300

    Exercises

---
+++ aula8_exercicio10.py
@@ -0,0 +1,18 @@
+'''
+Dado a lista de cliente abaixo. Crie uma função lambda para 
+  filtrar apenas os clientes com rating A e AA.
+'''
+clientes = [
+    {
+        'name': 'Alice',
+        'rating': 'AA',
+    },
+    {
+        'name': 'Bob',
+        'rating': 'A',
+    },
+    {
+        'name': 'Charlie',
+        'rating': 'B',
+    }
+]
